2b20f868788a7aa90f7da79602781e04e75c012b
rhf007 <rahofahazem512@gmail.com>
2023-11-24 14:58:18 -0800

    idk

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
 # installing a package
 package { 'flask':
-  ensure => 'installed',
-  install_options =>  ['-v', '2.1.0'],
+ command => '/usr/bin/pip3 install flask==2.1.0',
+}
+
+package { 'Werkzeug':
+  command => '/usr/bin/pip3 install werkzeug==2.1.1',
 }
