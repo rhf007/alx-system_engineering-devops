0134bca22bad5ec6433b0cec6a5580563bd328cd
rhf007 <rahofahazem512@gmail.com>
2023-11-24 14:43:56 -0800

    not the comma

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,8 @@
 file { '/tmp/school':
   ensure  => 'present',
   path    => '/tmp/school',
-  content => 'I love Puppet',`
+  content => 'I love Puppet',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
 }
