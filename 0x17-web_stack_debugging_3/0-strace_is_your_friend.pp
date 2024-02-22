748c6c110584358c3b2e6bc880c38d5f52d46da8
Rahaf Hazem <110139745+rhf007@users.noreply.github.com>
2024-02-23 00:35:51 +0200

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-#fixing apache 500 error
-
-exec { 'fixerror':
-  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  =>  "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
-  provider =>  'shell'}
